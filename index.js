9e5a3f0007c70b4196f9306d860b110c446f6786
Friis Kraas <friis.kraas@gmx.de>
2023-05-11 16:24:46 +0200

    added serverstart

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ const animalsRouter = require("./routes/animals.js");
 const PORT = process.env.PORT || 8080;
 
 app.use("/", animalsRouter);
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

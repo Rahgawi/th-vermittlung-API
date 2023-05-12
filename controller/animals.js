db1c7c2a0b4db3812344d6192491abea270fa05f
Friis Kraas <friis.kraas@gmx.de>
2023-05-12 16:06:29 +0200

    simplified data structure

--- controller/animals.js
+++ controller/animals.js
@@ -1,37 +1,17 @@
-const animals = {
-  items: [
-    {
-      fields: {
-        animalId: 104,
-        category: "Dog",
-        animalImage: {
-          sys: {
-            id: "4VScLyrFkPoNyvmWeGJhJm",
-          },
-        },
-        name: "Buddy",
-        gender: "male",
-        age: "0.5",
-        breed: "Golden Retriever",
-        tags: ["friendly", "affectionate"],
-        description: "Meet Buddy",
-      },
-    },
-  ],
-  includes: {
-    Asset: [
-      {
-        fields: {
-          title: "Buddy",
-          file: {
-            url: "//images.ctfassets.net/3buz4oreveso/2UeTeUce0ZCGlTAbPQPZ1g/381447e04fa04bbe38abc00b07e3b5f2/Azul.jpg",
-          },
-        },
-        sys: { id: "4VScLyrFkPoNyvmWeGJhJm" },
-      },
-    ],
+const animals = [
+  {
+    animalId: 104,
+    category: "Dog",
+    animalImageUrl:
+      "//images.ctfassets.net/3buz4oreveso/2UeTeUce0ZCGlTAbPQPZ1g/381447e04fa04bbe38abc00b07e3b5f2/Azul.jpg",
+    name: "Buddy",
+    gender: "male",
+    age: "0.5",
+    breed: "Golden Retriever",
+    tags: ["friendly", "affectionate"],
+    description: "Meet Buddy",
   },
-};
+];
 
 function getAllAnimals(req, res) {
   res.set("Access-Control-Allow-Origin", "*");
